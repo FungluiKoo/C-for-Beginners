ed9464c914ca0a85d97c707dc26e0fc53ca2dc51
Divide-et-impera-11 <54957167+Divide-et-impera-11@users.noreply.github.com>
2019-09-15 23:58:20 +0200

    Update int_char.c

--- Basic_Syntax_and_Functions/int_char.c
+++ Basic_Syntax_and_Functions/int_char.c
@@ -10,3 +10,10 @@ int main(){
     printf("0xa=%d, 012=%d, 0b100=%d.",baz16,baz8,baz2);
     return 0;
 }
+/*Hi! I'm new on GitHub.So I do programming in CPP and I wanted to improve my skills,this is why i decided i will jump in a open source project.
+I wanted to start with an easy. I searched by "friendly beginner" label and this is how I found you.
+#If i can help you somehow in a project.(some type of University project :)
+#If you have any program Idea (I have.)
+#Or just practice Git and Team Work, we should make something...It would be really great if i can practice Git/TeamWork/Programming with someone :)
+Sorry,I know you don't knwo me. but drop a message if you have mood for this... 
+Gmail:molnarmihaly11@gmail.com*/
